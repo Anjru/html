ddb99f6a9cfd0d92024f77a5810bfddd4bebdf8c
Anjru <andrew.yang2022@gmail.comgit config --global user.name Anjrugit config --global user.email andrew.yang2022@gmail.com>
2024-10-29 20:53:21 -0500

    movement

--- 2048/script.js
+++ 2048/script.js
@@ -32,6 +32,21 @@ function getRandom() {
     return Math.floor(Math.random() * 4); // Random number from 0 to 3
 }
 
+function newRandom(){
+    x = getRandom();
+    y = getRandom();
+
+    // Check if the position is null
+    if (arrayVal[x][y] === null) {
+        arrayVal[x][y] = 2; // Set value at random position
+    } else {
+        i--; // Decrement i to retry if position is occupied
+    }
+    update(); // Call update to show the changes
+    }
+
+
+
 function update() {
     for (let i = 0; i < 4; i++) { // Loop through 4 rows
         for (let j = 0; j < 4; j++) { // Loop through 4 columns
@@ -45,6 +60,11 @@ function boolReset() {
     for (let i = 0; i < 4; i++) { // Loop through 4 rows
         for (let j = 0; j < 4; j++) {
             arrayBool[i][j] = false; // Reset boolean array
+        }
+    }
+    for (let i = 0; i < 4; i++) { // Loop through 4 rows
+        for (let j = 0; j < 4; j++) {
+            arraySwitch[i][j] = false; // Reset boolean array
         }
     }
 }
@@ -69,49 +89,179 @@ document.addEventListener('keydown', function(event) {
     }
 });
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function wait() {
+    await sleep(2000);
+    
+}
+
 function up(){
-    for(var j = 1; j < 4; j++){
-        for(var i = 0; i < 4; i++){
+    boolReset();
+    //animate up 3 times max
+    for(var k = 0; k < 3; k++){
+    for(var i = 0; i < 4; i++){
+        for(var j = 1; j < 4; j++){
+            //Current NOT NULL
             if(arrayVal[j][i] != null){
-                //Row above
+                //NEXT NULL
                 if(arrayVal[j-1][i] == null){
-                    //if row towards edge is null
                     var tempVal = arrayVal[j][i];
                     arrayVal[j][i] = null;
                     arrayVal[j-1][i] = tempVal;
-                    
-                    arrayBool
 
-                } else {
-                    //if row towards edge is not null
-                    //Only combine nothing has combined there yet
-                    //if they're the same, combine
-                    if(arraySwitch[j-1][i] != true){
+                } else { // NEXT NOT NULL
+                    //CHECK COMBINE
+                    if(arraySwitch[j-1][i] == false){
+                        //COMBINE
                         if(arrayVal[j-1][i] == arrayVal[j][i]){
-                            arrayVal[j-i][i] *= 2;
+                            arrayVal[j-1][i] *= 2;
                             arrayVal[j][i] = null;
                             arraySwitch[j-1][i] = true;
+                            
+                            arrayBool[j][i] = true;
                         }
                     }
-                    //if not, stop
+                    //Can't Combine
                     else{
-
+                        
                     }
 
                 }
             }
         }
     }
+
+    update();
+    wait();
+    }
+    //Should only random if can move or combine FIX!!!!!!!!!!!!!!!!!!!!!!!
+    newRandom();
+    
 }
 
 function down(){
+    boolReset();
+    //animate down 3 times max
+    for(var k = 0; k < 3; k++){
+    for(var i = 0; i < 4; i++){
+        for(var j = 2; j >= 0; j--){
+            //Current NOT NULL
+            if(arrayVal[j][i] != null){
+                //NEXT NULL
+                if(arrayVal[j+1][i] == null){
+                    var tempVal = arrayVal[j][i];
+                    arrayVal[j][i] = null;
+                    arrayVal[j+1][i] = tempVal;
 
+                } else { // NEXT NOT NULL
+                    //CHECK COMBINE
+                    if(arraySwitch[j+1][i] == false){
+                        //COMBINE
+                        if(arrayVal[j+1][i] == arrayVal[j][i]){
+                            arrayVal[j+1][i] *= 2;
+                            arrayVal[j][i] = null;
+                            arraySwitch[j+1][i] = true;
+                            
+                            arrayBool[j][i] = true;
+                        }
+                    }
+                    //Can't Combine
+                    else{
+                        
+                    }
+
+                }
+            }
+        }
+    }
+
+    update();
+    wait();
+    }
 }
 
 function left(){
+    boolReset();
+    //animate up 3 times max
+    for(var k = 0; k < 3; k++){
+    for(var i = 1; i < 4; i++){
+        for(var j = 0; j < 4; j++){
+            //Current NOT NULL
+            if(arrayVal[j][i] != null){
+                //NEXT NULL
+                if(arrayVal[j][i-1] == null){
+                    var tempVal = arrayVal[j][i];
+                    arrayVal[j][i] = null;
+                    arrayVal[j][i-1] = tempVal;
 
+                } else { // NEXT NOT NULL
+                    //CHECK COMBINE
+                    if(arraySwitch[j][i-1] == false){
+                        //COMBINE
+                        if(arrayVal[j][i-1] == arrayVal[j][i]){
+                            arrayVal[j][i-1] *= 2;
+                            arrayVal[j][i] = null;
+                            arraySwitch[j][i-1] = true;
+                            
+                            arrayBool[j][i] = true;
+                        }
+                    }
+                    //Can't Combine
+                    else{
+                        
+                    }
+
+                }
+            }
+        }
+    }
+
+    update();
+    wait();
+    }
 }
 
 function right(){
+    boolReset();
+    //animate up 3 times max
+    for(var k = 0; k < 3; k++){
+    for(var i = 2; i >= 0; i--){
+        for(var j = 0; j < 4; j++){
+            //Current NOT NULL
+            if(arrayVal[j][i] != null){
+                //NEXT NULL
+                if(arrayVal[j][i+1] == null){
+                    var tempVal = arrayVal[j][i];
+                    arrayVal[j][i] = null;
+                    arrayVal[j][i+1] = tempVal;
 
+                } else { // NEXT NOT NULL
+                    //CHECK COMBINE
+                    if(arraySwitch[j][i+1] == false){
+                        //COMBINE
+                        if(arrayVal[j][i+1] == arrayVal[j][i]){
+                            arrayVal[j][i+1] *= 2;
+                            arrayVal[j][i] = null;
+                            arraySwitch[j][i+1] = true;
+                            
+                            arrayBool[j][i] = true;
+                        }
+                    }
+                    //Can't Combine
+                    else{
+                        
+                    }
+
+                }
+            }
+        }
+    }
+
+    update();
+    wait();
+    }
+    //newRandom();
 }
